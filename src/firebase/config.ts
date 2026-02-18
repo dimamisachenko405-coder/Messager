cc7a4a721c7101c7df13c1fac3057b64006538aa
dima misachenko <dimamisachenko405@gmail.com>
2026-02-18 08:00:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3988930043-5442a",
+  "appId": "1:837317003857:web:c12c481ff1ac180d01c670",
+  "apiKey": "AIzaSyAIL9z4A-ZaGvCtZWZgqu4wGfxCRxkHQYI",
+  "authDomain": "studio-3988930043-5442a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "837317003857"
+};
